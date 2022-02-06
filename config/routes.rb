23becbcc0454153297fa1348f61948468ac5e3a9
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2022-02-06 10:18:42 +0900

    restart

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get '/', to: 'top#index'
+  get '/users', to: 'users#index', as: "users"
+
+  # /users/1
+  # /users/2
+  get '/users/:id', to: 'users#show', as: "user"
+end
